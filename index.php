a7b331b62b2e703795ed415d38ba5a6bc03b549f
Gerasimov Andrey <andrey.gerasimov2506@gmail.com>
2022-04-27 20:55:37 +0300

    Gerasimov Andrey

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Окна в дом - пластиковые окна на заказ в Пензе</title>
+        <meta name="description" content="В нашей компании вы можете заказать окна, балконы, лоджии с максимальной выгодой. Мы обязательно учтем все ваши пожелания!">
+        <meta name="keywords" content="Пластиковые окна, Заказать окно в Пензе, Окна в дом, Пенза окна, Окна на заказ, Хорошие пластиковые окна, Окно на дачу">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+    </head>
+    <body>
+        <section class="preheader_section">
+            <div class="wrapper">
+                <div class="preheader">
+                    <p class="p_p"><span><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i></span> г. Пенза ул. Коммунистическая, 28</p>
+                    <p class="p_p"><span><i class="fa fa-phone fa-fw" aria-hidden="true"></i></span> 8 (8412) 25-16-10, 8 (937) 416-16-10</p>
+                </div>
+            </div>
+        </section>
+        <header>
+            <div class="wrapper">
+                <div class="header">
+                    <img class="h_img" src="img/logo.png" alt="Logo">
+                    <nav class="h_nav">
+                        <a class="hn_a" href="#">Новости</a>
+                        <a class="hn_a" href="#">Профильные системы</a>
+                        <a class="hn_a" href="#">Наши услуги</a>
+                        <a class="hn_a" href="#">Портфолио</a>
+                        <a class="hn_button" href="#">Рассчитать стоимость</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
