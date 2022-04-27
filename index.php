23a5d6dfd9208f9002516a2420cc2d55bd3afafd
Gerasimov Andrey <andrey.gerasimov2506@gmail.com>
2022-04-27 21:42:18 +0300

    Gerasimov Andrey

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 <html lang="ru">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Окна в дом - пластиковые окна на заказ в Пензе</title>
         <meta name="description" content="В нашей компании вы можете заказать окна, балконы, лоджии с максимальной выгодой. Мы обязательно учтем все ваши пожелания!">
         <meta name="keywords" content="Пластиковые окна, Заказать окно в Пензе, Окна в дом, Пенза окна, Окна на заказ, Хорошие пластиковые окна, Окно на дачу">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     </head>
     <body>
+        <section class="menu menu_off" onclick="menu_up()">
+            <div class="m_menu">
+                <a class="m_a" href="#">Новости</a>
+                <a class="m_a" href="#">Профильные системы</a>
+                <a class="m_a" href="#">Наши услуги</a>
+                <a class="m_a" href="#">Портфолио</a>
+                <a class="m_a" href="#">Рассчитать стоимость</a>
+            </div>
+        </section>
         <section class="preheader_section">
             <div class="wrapper">
                 <div class="preheader">
@@ -20,7 +31,7 @@
         <header>
             <div class="wrapper">
                 <div class="header">
-                    <img class="h_img" src="img/logo.png" alt="Logo">
+                    <a href="/"><img class="h_img" src="img/logo.png" alt="Logo"></a>
                     <nav class="h_nav">
                         <a class="hn_a" href="#">Новости</a>
                         <a class="hn_a" href="#">Профильные системы</a>
@@ -28,8 +39,33 @@
                         <a class="hn_a" href="#">Портфолио</a>
                         <a class="hn_button" href="#">Рассчитать стоимость</a>
                     </nav>
+                    <a class="m_h_button_menu" href="#" onclick="menu_down()"><i class="fa fa-bars fa-fw" aria-hidden="true"></i></a>
                 </div>
             </div>
         </header>
+        <section class="welcome_section">
+            <div class="wrapper">
+                <div class="welcome">
+                    <p class="w_p">Сохраним тепло в вашем доме!</p>
+                    <h1 class="w_h1">Окна, балконы, лоджии с большими скидками.</h1>
+                    <a class="w_a" href="#">Хочу консультацию</a>
+                </div>
+            </div>
+        </section>
+        <script>
+            function menu_down() {
+                $('.menu').slideToggle(300, function(){
+                    $('.menu').removeClass('menu_off');
+                    $('.menu').addClass('menu_on');
+                });
+            }
+
+            function menu_up() {
+                $('.menu').slideToggle(300, function(){
+                    $('.menu').removeClass('menu_off');
+                    $('.menu').addClass('menu_on');
+                });
+            }
+        </script>
     </body>
 </html>
