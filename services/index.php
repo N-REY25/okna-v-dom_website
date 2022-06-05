2e9fb1801e6dc8b2b47eee71edcba578ae27f234
Andrey Gerasimov <andrey.gerasimov2506@gmail.com>
2022-06-05 13:32:29 +0300

    Andrey Gerasimov

--- services/index.php
+++ services/index.php
@@ -14,17 +14,27 @@
         <section class="welcome_section">
             <div class="wrapper">
                 <div class="welcome">
-                    <h1 class="w_h1 w_h1_color">Наши услуги</h1>
+                    <h1 class="w_h1">Наши услуги</h1>
                     <img class="w_img" src="img/welcome_2.png" alt="welcome_2">
                 </div>
             </div>
         </section>
 
-        <!-- Services -->
+        <!-- Services page -->
         <section class="services_page_section">
             <div class="wrapper">
                 <div class="services_page">
-                    
+                    <div class="serp_item">
+                        <img class="serpi_img" src="/img/services/img_1.png">
+                        <div class="serpi_info">
+                            <div class="serpi_text">
+                                <h2>Окна в дом или квартиру</h2>
+                                <p>От 2300 ₽за м2</p>
+                                <p>Каким критериям должны соответствовать пластиковые окна для дома или квартиры?<br><br>• шумоизоляция<br>• теплоизоляция<br>• эстетичный<br>• внешний вид<br>• долговечность<br><br>Каждое окно проходит контроль качества в соответствии с ГОСТом. Широкий выбор: от бюджетного до премиум класса. Гарантийное обслуживание в течении 5 лет.<br>Понимая необходимость замены окон, мы снизили цены для вас!</p>
+                            </div>
+                            <a class="serpi_a" href="#">Заказать</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
