7697cc142b995f0f81a77c8c9921f27e25141d00
Andrey Gerasimov <andrey.gerasimov2506@gmail.com>
2022-06-05 13:02:29 +0300

    Andrey Gerasimov

--- services/index.php
+++ services/index.php
@@ -4,27 +4,34 @@
         <title>Окна в дом - пластиковые окна на заказ в Пензе</title>
         <meta name="description" content="В нашей компании вы можете заказать окна, балконы, лоджии с максимальной выгодой. Мы обязательно учтем все ваши пожелания!">
         <!-- Meta -->
-        <?php require '/orm/meta.php'; ?>
+        <?php require '../orm/meta.php'; ?>
     </head>
     <body>
         <!-- Header -->
-        <?php require '/orm/header.php'; ?>
+        <?php require '../orm/header.php'; ?>
 
         <!-- Welcome -->
         <section class="welcome_section">
             <div class="wrapper">
                 <div class="welcome">
-                    <p class="w_p">Сохраним тепло в вашем доме!</p>
-                    <h1 class="w_h1">Окна, балконы, лоджии с большими скидками.</h1>
-                    <a class="w_a" href="#">Получить консультацию</a>
+                    <h1 class="w_h1 w_h1_color">Наши услуги</h1>
                     <img class="w_img" src="img/welcome_2.png" alt="welcome_2">
                 </div>
             </div>
         </section>
 
+        <!-- Services -->
+        <section class="services_page_section">
+            <div class="wrapper">
+                <div class="services_page">
+                    
+                </div>
+            </div>
+        </section>
+
         <!-- Footer -->
-        <?php require '/orm/footer.php'; ?>
+        <?php require '../orm/footer.php'; ?>
         <!-- Script -->
-        <?php require '/orm/script.php'; ?>
+        <?php require '../orm/script.php'; ?>
     </body>
 </html>
